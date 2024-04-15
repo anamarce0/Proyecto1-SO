cd0c0ee944420a0c95ccc82ecc648ed23c749cc0
anamarce0 <131510302+anamarce0@users.noreply.github.com>
2024-04-14 22:29:14 -0600

    Update memory.c

--- memory.c
+++ memory.c
@@ -9,10 +9,10 @@ int main(int argc, char *argv[]) {
     FILE *fp;
     char buffer[MAX_BUFFER_SIZE];
     double memoria_total = 0.0, memoria_usada = 0.0, porcentaje = 0.0, memVirtual, memReal;
-    char parametros[MAX_BUFFER_SIZE] = " ";
+    char parametros[MAX_BUFFER_SIZE] = " " ;
 
     char *unit = argv[1];
-    if (argc == 2) {
+    if (argc == 2){
         if (strcmp(unit, "-r") == 0) {
             fp = popen("top -b -n 1| grep 'MiB Mem'", "r");
             if (fp == NULL) {
@@ -25,6 +25,7 @@ int main(int argc, char *argv[]) {
 
             porcentaje = (memoria_usada / memoria_total) * 100;
 
+        
             sprintf(parametros, "PORCENTAJE:%.2f%%\n", porcentaje);
             write(STDOUT_FILENO, parametros, strlen(parametros));
 
@@ -40,15 +41,17 @@ int main(int argc, char *argv[]) {
 
             porcentaje = (memoria_usada / memoria_total) * 100;
 
+
             sprintf(parametros, "PORCENTAJE:%.2f%%\n", porcentaje);
             write(STDOUT_FILENO, parametros, strlen(parametros));
-        }
-    } else if (argc == 3) {
+        } 
+    } else if (argc == 3){
         int pid = atoi(argv[2]);
         char command[MAX_BUFFER_SIZE];
-        if (strcmp(unit, "-v") == 0) {
-            snprintf(command, sizeof(command), "top -bn1 -p %d -c | awk 'NR>7 && $1==%d {print $12, $5/1024}'", pid, pid);
+        if (strcmp(unit, "-v") == 0) {   
+            snprintf(command, sizeof(command), "top -bn1 -p %d | awk 'NR>7 && $1==%d {print $12, $5/1024}'", pid, pid);
 
+                
             fp = popen(command, "r");
             if (fp == NULL) {
                 sprintf(parametros, "Error al ejecutar el comando top\n");
@@ -59,6 +62,7 @@ int main(int argc, char *argv[]) {
             char nombre[25] = "";
             if (fgets(buffer, sizeof(buffer), fp) != NULL) {
                 sscanf(buffer, "%s  %lf", nombre, &memVirtual);
+                   
             }
 
             fp = popen("free -m | awk 'NR==3{printf \"%.2f\\n\", ($2)}'", "r");
@@ -67,18 +71,20 @@ int main(int argc, char *argv[]) {
                 write(STDOUT_FILENO, parametros, strlen(parametros));
                 return -1;
             }
+                
 
             if (fgets(buffer, sizeof(buffer), fp) != NULL) {
                 sscanf(buffer, "%lf", &memoria_total);
+
             }
 
-            porcentaje = ((memVirtual) / memoria_total) * 100;
+            porcentaje = ((memVirtual)/memoria_total) *100;
 
             sprintf(parametros, "PID: %d\nNOMBRE: %s\nPORCENTAJE DE MEMORIA VIRTUAL: %.2f%%\n", pid, nombre, porcentaje);
             write(STDOUT_FILENO, parametros, strlen(parametros));
         } else {
             snprintf(command, sizeof(command), "top -bn1 -p %d | awk 'NR>7 && $1==%d {print $12, $10}'", pid, pid);
-
+                
             fp = popen(command, "r");
             if (fp == NULL) {
                 sprintf(parametros, "Error al ejecutar el comando top\n");
@@ -88,16 +94,16 @@ int main(int argc, char *argv[]) {
 
             char nombre[25] = "";
             if (fgets(buffer, sizeof(buffer), fp) != NULL) {
-                sscanf(buffer, "%s %lf", nombre, &memReal);
+                sscanf(buffer, "%s %lf",nombre, &memReal);
+            
             }
 
             porcentaje = memReal;
-
+                
             sprintf(parametros, "PID: %d\nNOMBRE:%s\nPORCENTAJE DE MEMORIA :%.2f%%\n", pid, nombre, porcentaje);
             write(STDOUT_FILENO, parametros, strlen(parametros));
-        }
+        }   
     }
 
     return 0;
 }
-
